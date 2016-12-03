551ff9e3cc93d9d031df80a21c5b9f4c07b0a508
Heri Agape <agape@live.fr>
2016-12-04 00:42:17 +0300

    added the folder structure of the other modules

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         $this->call(RolesTableSeeder::class);
+        $this->command->info('Roles ans Permissions seeded! \0/');
 
     }
 }
